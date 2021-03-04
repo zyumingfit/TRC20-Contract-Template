42ea5b1b268b90480bccfb6e8ad2f3255caea3fb
coinwaycan <71204887+coinwaycan@users.noreply.github.com>
2021-03-04 21:15:45 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("LIGHTNING FINANCE", "ODI", 18) {
+    constructor () public ERC20Detailed("LIGHTNING FINANCE", "LGF", 18) {
         _mint(msg.sender, 600000000 * (10 ** uint256(decimals())));
     }
 }
