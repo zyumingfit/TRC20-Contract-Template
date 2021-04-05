62ec80283a294236745af4393d533cb2d31be4c8
criptocomunidad <80938467+criptocomunidad@users.noreply.github.com>
2021-04-04 21:56:16 -0500

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("Mexican"
-        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("MexicanPesoToken" , "MXNT", 18) {
+    _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
     }
 }
