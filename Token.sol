7610b9cebe8470f7ca9a30c64367db2ec70dd6bd
AryAArd <90168868+AryAArd@users.noreply.github.com>
2021-09-06 08:58:39 +0430

    Revert "1scoin.sol"
    This reverts commit 9e6193abe11c531c3a96aefea04575441a0b1ddf.

--- Token.sol
+++ Token.sol
@@ -2,21 +2,21 @@
 // Enable optimization
 pragma solidity ^0.5.0;
 
-import "./TRC20.sol";
-import "./TRC20Detailed.sol";
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
 
 /**
- * @title 1SportCoin
- * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
- * `TRC20` functions.
+ * `ERC20` functions.
  */
-contract Token is TRC20, TRC20Detailed {
+contract Token is ERC20, ERC20Detailed {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("1SportCoin", "1SC", 18) {
-        _mint(msg.sender, 1000000000000000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("YourTokenName", "YTN", 18) {
+        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
     }
 }
