32a727554a20d19b7f8e9659fe80584f720f995e
Astitv Singh <98196550+proudstoner@users.noreply.github.com>
2022-01-31 15:41:24 +0530

    Updated `Token.sol` for changes regarding `TRC20`.
    The changes made in this commit are as follows :
    ~ Updated the corresponding `@dev` note accordingly for the updated contract.
    ~ Updated the corresponding `imports` accordingly for the updated contract.
    ~ Updated the corresponding contract.

--- Token.sol
+++ Token.sol
@@ -2,21 +2,21 @@
 // Enable optimization
 pragma solidity ^0.5.0;
 
-import "./ERC20.sol";
-import "./ERC20Detailed.sol";
+import "./TRC20.sol";
+import "./TRC20Detailed.sol";
 
 /**
  * @title SimpleToken
- * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` functions.
+ * `TRC20` functions.
  */
-contract Token is ERC20, ERC20Detailed {
+contract Token is TRC20, TRC20Detailed {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("YourTokenName", "YTN", 18) {
+    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
         _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
     }
 }
