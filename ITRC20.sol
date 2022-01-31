01d2c334633291f31b192bfafcc14063cac42585
Astitv Singh <98196550+proudstoner@users.noreply.github.com>
2022-01-31 13:38:56 +0530

    Rename IERC20.sol to ITRC20.sol
    The changes made in this commit are as follows :
    ~ Rename of file from `IERC20.sol` to `ITRC20.sol`.
    ~ Rename of interface contract from `IERC20` to `ITRC20`.
    ~ Updated the corresponding `@dev` note accordingly for the updated interface contract.

--- ITRC20.sol
+++ ITRC20.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.5.0;
 
 /**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see {ERC20Detailed}.
+ * @dev Interface of the TRC20 standard as defined in the TIP. Does not include
+ * the optional functions; to access them see {TRC20Detailed}.
  */
-interface IERC20 {
+interface ITRC20 {
     /**
      * @dev Returns the amount of tokens in existence.
      */
